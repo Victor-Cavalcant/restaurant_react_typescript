74ff4da20f37625968e9f0942e057b5a7f5f40a1
Typ-0 <142792234+Typ-0@users.noreply.github.com>
2025-02-10 16:07:38 -0300

    quick fix

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,24 +3,23 @@ import Button from "../components/Button";
 function Home() {
     return (
         <>
-            <div className="bg-[url('/Users/victorcavalcanti/Desktop/projetos/type/public/images/spices.jpg')] bg-cover h-screen flex justify-center"  >
+            <div className="bg-[url('/Users/victorcavalcanti/Desktop/projetos/type/public/images/spices.jpg')] bg-cover h-screen flex flex-col "  >
+                <div className='grid justify-center gap-2 sm:flex sm:flex-row sm:gap-30 p-6'>
+                    <Button path="/" name='Home' />
+                    <Button path="/menu" name='Menu' />
+                    <Button path="/about" name='About' />
+                </div>
+                <div>
 
-                <div className='flex flex-col items-center sm:items-start text-center sm:text-left'>
-                    <div className='flex flex-col sm:flex-row gap-4 sm:gap-30 p-6'>
-                        <Button path="/"  name='Home' />
-                        <Button path="/menu" name='Menu' />
-                        <Button path="/about"  name='About' />
+
+                    <div className="sm:mt-12">
+                        <h1 className="mx-auto text-4xl sm:text-4xl mt-24 sm:w-[380px] w-80 text-center font-bold text-white bg-black/70 rounded-full p-2">Aroma Restaurant</h1>
+                        <p className="mx-auto mt-60 w-96 text-2xl text-center font-bold text-white bg-black/70 rounded-full p-2">An esquisite culinary experience</p>
                     </div>
-                    <p
-                        className='bg-black/50 mt-6 sm:mt-12 px-6 py-2 w-auto text-white text-3xl sm:text-5xl font-bold rounded-full mr-auto ml-auto'
-                    >Aroma Restaurant</p>
-                    <p
-                        className='bg-black/50 mt-[40vh] px-4 py-2 w-full max-w-lg text-white text-center sm:text-xl font-bold rounded-full mr-auto ml-auto'
-                    >An exquisite culinary experience</p>
+
                 </div>
             </div>
         </>
     );
 }
-
 export default Home
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,16 +2,16 @@ import Button from "../components/Button";
 function Menu() {
     return (
         <>
-            <div className="bg-[url('/Users/victorcavalcanti/Desktop/projetos/type/public/images/spices.jpg')] bg-cover h-screen flex flex-col "  >
+            <div className="bg-[url('/Users/victorcavalcanti/Desktop/projetos/type/public/images/spices.jpg')] bg-cover h-[130vh] flex flex-col sm:h-screen "  >
                 <div className='grid justify-center gap-2 sm:flex sm:flex-row sm:gap-30 p-6'>
                     <Button path="/" name='Home' />
                     <Button path="/menu" name='Menu' />
                     <Button path="/about" name='About' />
                 </div>
-                <div className="mt-auto mb-auto">
+                <div>
 
 
-                    <div className="grid h-[90vh] p-2 sm:flex sm:mt-[12vh] sm:w-full">
+                    <div className="grid h-[90vh] p-2 sm:flex sm:justify-center sm:items-center">
                         <div className="w-full mx-auto p-2">
                             <p className="mx-auto text-2xl bg-black/70 w-24 text-center text-white h-8 mt-4 rounded-full my-2 sm:gap-2">Brunch</p>
                             <div className="w-full mx-auto h-60 shadow-lg bg-[url('/images/brunch.jpg')] rounded-4xl  bg-contain bg-center bg-no-repeat sm:h-90 sm:w-[30vw]"></div>
